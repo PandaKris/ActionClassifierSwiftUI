0d1571263c0c5dcaf10e73ab510891923d7f482f
Kristanto Sean <kristantosean@gmail.com>
2024-07-18 08:17:37 +0700

    Initial Commit

---
+++ ActionClassifierSwiftUI/ActionClassifierSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionClassifierSwiftUIApp.swift
+//  ActionClassifierSwiftUI
+//
+//  Created by Kristanto Sean on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ActionClassifierSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
